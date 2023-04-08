0296ed1b190e237e147d856645962c4b0381c563
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2023-04-09 01:05:52 +0530

    feat: user model, signupfun without aadhaar validation

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class sathiUser(AbstractUser):
+    phone = models.CharField(max_length=15)
+    aadhaarno = models.CharField(max_length=12)
+    pass
--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import requests
+from django.http import JsonResponse
+from .models import sathiUser
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
 def index(request):
@@ -8,4 +13,32 @@ def loginfun(request):
     return render(request,"login.html")
 
 def signupfun(request):
+    if request.method == 'POST':
+        full_name = request.POST['full_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        phone = request.POST['phone']
+        gender = request.POST['gender']
+        first_name = full_name.split()[0]
+        last_name = full_name.split()[1]
+        addhaar = request.POST['addhaar']
+        password = request.POST['password']
+        confirm_password = request.POST['cpassword']
+        
+        # after varification of aadhaar
+        
+        # check if user already exists
+        if sathiUser.objects.filter(username=username).exists():
+            return JsonResponse({'message': 'User already exists'}, status=400)
+        
+        # save user to database
+        newUser = sathiUser.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name, phone=phone, aadhaarno=addhaar)
+        
+        # login user
+        try:
+            login(request, newUser)
+            return JsonResponse({'message': 'User registered successfully'}, status=200)
+        except:
+            return JsonResponse({'message': 'Error while registering user'}, status=400)
+        
     return render(request,"signup.html")
