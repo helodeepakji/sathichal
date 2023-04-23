0f213e3353392b3ddd2f80e89b2414a3e6ead671
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2023-04-23 12:22:05 +0530

    fix: nav links in route.html

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home', views.index, name='index'),
     path('routing', views.routing, name='routing'),
     path('login', views.loginfun, name='login'),
     path('signup', views.signupfun, name='signup'),  
