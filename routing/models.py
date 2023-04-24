c55bc509f538f542c783e1addb7cc922b8e855e3
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2023-04-24 17:58:47 +0530

    feat: confirm user structure

--- routing/models.py
+++ routing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from home.models import sathiUser
 # Create your models here.
+class group(models.Model):
+    # jo user add hua
+    added_user = models.CharField(max_length=100)
+    # jis user ne add kiya / jis user nei confirm kiya
+    added_by_user = models.CharField(max_length=100)
+    # self.room_group_name
+    group_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
