50ede0736cff82ea7d240c7b6ac87c8962fe9f54
Tarun Garg <96339593+Evil2004@users.noreply.github.com>
2023-05-01 23:05:58 +0530

    fix: users in group bug

--- routing/consumers.py
+++ routing/consumers.py
@@ -82,23 +82,32 @@ class routConsumer(AsyncWebsocketConsumer):
         
         # confirm user event type
         if eventType == 'sendConfirm':
-            
+            print(self.room_group_name)
             date = datetime.now().date()
             time = datetime.now().time()
-            sathiid = text_data_json['sathiID']
+            sathi_id = text_data_json['sathiID']
+            temp_sathi_id = None
+            # if sathi id is empty and group is already exist for sender or reciver then use that group id
+            if sathi_id == '' and await database_sync_to_async(group.objects.filter(Q(added_by_user=text_data_json['sender']) | Q(added_user=text_data_json['reciver']),status='P').exists)():
+                temp_sathi_id = await database_sync_to_async(group.objects.filter(added_by_user=text_data_json['sender'],status='P'))()
+                sathi_id = temp_sathi_id[0].sathi_id
+                print(temp_sathi_id)
+            
             # if sathi id is not empty
-            if sathiid == '':
+            if temp_sathi_id == None:
                 sathiid = "SATHI"+str(random.randint(10000, 99999))
-                is_sathi_id = await database_sync_to_async(group.objects.filter(sathi_id=sathiid).exists)()
-                print("is sathi id",is_sathi_id)
                 # if sathi id is already exist
-                while is_sathi_id == True:
-                    print("sathi id",sathiid)
+                while await database_sync_to_async(group.objects.filter(sathi_id=sathiid,status='P').exists)():
+                    print("sathi id in while loop",sathiid)
                     sathiid = "SATHI"+str(random.randint(10000, 99999))
+                sathi_id = sathiid
+            else:
+                sathi_id = temp_sathi_id
+
             
-            
+            print("sathi id before saving group",sathiid)
             # to save group
-            created_group = group(group_name=self.room_group_name, added_by_user=text_data_json['sender'], added_user=text_data_json['reciver'], status = 'P' ,date=date, time=time, sathi_id=sathiid)
+            created_group = group(group_name=self.room_group_name, added_by_user=text_data_json['sender'], added_user=text_data_json['reciver'], status = 'P' ,date=date, time=time, sathi_id=sathi_id)
             await database_sync_to_async(created_group.save)()
             
             print("group created",created_group)
